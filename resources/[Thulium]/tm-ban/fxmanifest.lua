2db693a75cecea97315847af3f8b62610efd3f2e
PenT1x <68808737+PenT1x@users.noreply.github.com>
2022-08-14 17:11:46 +0200

    Update fxmanifest.lua

--- resources/[Thulium]/tm-ban/fxmanifest.lua
+++ resources/[Thulium]/tm-ban/fxmanifest.lua
@@ -2,16 +2,12 @@ fx_version 'cerulean'
 game 'gta5'
 
 author 'Thulium.dev'
-description "Bans players if there are in Thulium.dev's modder database"
+description "Bans players if there are in CitizenBan's modder database"
 version '0.1.0'
 
 server_scripts {
     'config.lua',
     'server.lua'
-}
-
-escrow_ignore {
-    'config.lua'
 }
 
 server_only 'yes'
